6245e1a6fc531f5c0a288f6c453de955dbf83f81
anacristinalr <anacristina.lr24@gmail.com>
2025-05-25 14:19:51 -0500

    Endpoints en HomeController y creacion de login y register

--- src/main/java/com/magicshop/ecommerce/controller/HomeController.java
+++ src/main/java/com/magicshop/ecommerce/controller/HomeController.java
@@ -1,5 +1,26 @@
 package com.magicshop.ecommerce.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
     
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 }
